b24f751a6d8433d64fdcdb04cc9d2a85dd919b90
Oleksii Salmin <salminalex2012@gmail.com>
2026-08-11 22:33:02 -0600

    use AceButton to better do buttons

--- coffe-grinder/coffe-grinder/coffe-grinder.ino
+++ coffe-grinder/coffe-grinder/coffe-grinder.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <AceButton.h>
+
+using namespace ace_button;
 
 #define BTN_START   A3
 #define BTN_MINUS   A2
@@ -16,24 +19,41 @@
 
 Adafruit_SSD1306 display(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
+ButtonConfig cfgStart;
+AceButton btnStart(&cfgStart, BTN_START);
+AceButton btnPlus(BTN_PLUS);
+AceButton btnMinus(BTN_MINUS);
+
 int value = 0;
+bool dirty = true;
 
-struct Button {
-  uint8_t pin;
-  bool    last;
-  unsigned long lastMs;
-};
+unsigned long flashUntil = 0;
+int flashPin = LED_START;
 
-Button bStart = {BTN_START, HIGH, 0};
-Button bPlus  = {BTN_PLUS,  HIGH, 0};
-Button bMinus = {BTN_MINUS, HIGH, 0};
+void flash(int pin) {
+  flashPin   = pin;
+  flashUntil = millis() + 150;
+}
 
-bool pressed(Button &b) {
-  bool now = digitalRead(b.pin);
-  bool hit = (now == LOW && b.last == HIGH && millis() - b.lastMs > 50);
-  if (hit) b.lastMs = millis();
-  b.last = now;
-  return hit;
+void handleEvent(AceButton* b, uint8_t event, uint8_t state) {
+  bool fire = (event == AceButton::kEventPressed ||
+               event == AceButton::kEventRepeatPressed);
+
+  switch (b->getPin()) {
+    case BTN_PLUS:
+      if (fire) { value++; flash(LED_PLUSMIN); dirty = true; }
+      break;
+    case BTN_MINUS:
+      if (fire) { value--; flash(LED_PLUSMIN); dirty = true; }
+      break;
+    case BTN_START:
+      if (event == AceButton::kEventReleased) {
+        value = 0;
+        flash(LED_START);
+        dirty = true;
+      }
+      break;
+  }
 }
 
 void draw() {
@@ -55,14 +75,6 @@ void draw() {
   display.display();
 }
 
-unsigned long flashUntil = 0;
-int flashPin = LED_START;
-
-void flash(int pin) {
-  flashPin   = pin;
-  flashUntil = millis() + 150;
-}
-
 void setup() {
   pinMode(BTN_START,   INPUT_PULLUP);
   pinMode(BTN_PLUS,    INPUT_PULLUP);
@@ -70,18 +82,28 @@ void setup() {
   pinMode(LED_START,   OUTPUT);
   pinMode(LED_PLUSMIN, OUTPUT);
 
+  ButtonConfig* cfg = ButtonConfig::getSystemButtonConfig();
+  cfg->setEventHandler(handleEvent);
+  cfg->setFeature(ButtonConfig::kFeatureRepeatPress);
+  cfg->setRepeatPressDelay(400);
+  cfg->setRepeatPressInterval(120);
+
+  cfgStart.setEventHandler(handleEvent);
+  cfgStart.setFeature(ButtonConfig::kFeatureLongPress);
+  cfgStart.setFeature(ButtonConfig::kFeatureSuppressAfterLongPress);
+
   display.begin(SSD1306_SWITCHCAPVCC);
-  draw();
 }
 
 void loop() {
-  bool changed = false;
+  btnStart.check();
+  btnPlus.check();
+  btnMinus.check();
 
-  if (pressed(bPlus))  { value++;   flash(LED_PLUSMIN); changed = true; }
-  if (pressed(bMinus)) { value--;   flash(LED_PLUSMIN); changed = true; }
-  if (pressed(bStart)) { value = 0; flash(LED_START);   changed = true; }
-
-  if (changed) draw();
+  if (dirty) {
+    draw();
+    dirty = false;
+  }
 
   if (millis() < flashUntil) {
     analogWrite(flashPin, 255);
@@ -90,6 +112,4 @@ void loop() {
     analogWrite(LED_START,   40);
     analogWrite(LED_PLUSMIN, 40);
   }
-
-  delay(10);
 }
